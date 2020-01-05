cd09ff4b9365b5e672cb8ddab69d44ba4f7ce8e2
PadmaPrasad <padmaprasad91@gmail.com>
2020-01-05 20:12:22 +0530

    springboot-buildingblocks

---
+++ springboot-buildingblock/src/test/java/com/myapp/springbootrest/SpringbootBuildingblockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.springbootrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBuildingblockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
