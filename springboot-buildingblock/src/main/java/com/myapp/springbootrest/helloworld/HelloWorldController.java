471e5dc3b977169c9c5b1f3c5afbe5d5e921dd5f
PadmaPrasad <padmaprasad91@gmail.com>
2020-02-10 20:19:40 +0530

    Springboot internationalization

--- springboot-buildingblock/src/main/java/com/myapp/springbootrest/helloworld/HelloWorldController.java
+++ springboot-buildingblock/src/main/java/com/myapp/springbootrest/helloworld/HelloWorldController.java
@@ -1,12 +1,18 @@
 package com.myapp.springbootrest.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+	private ReloadableResourceBundleMessageSource messageSource;
 
 	@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
 	public String helloworld() {
@@ -17,10 +23,16 @@ public class HelloWorldController {
 	public String helloSpringBoot() {
 		return "Welcome to Springboot";
 	}
-	
+
 	@GetMapping("/hellodetails")
 	public UserDetails helloDetails() {
 		return new UserDetails("Padma", "Prasad", "Bangalore");
 	}
-	
+
+	@GetMapping("/hello-int")
+	public String getMessageInI18NFormat(@RequestHeader(name = "Accept-Language", required = false) String locale) {
+
+		return messageSource.getMessage("label.hello", null, new Locale(locale));
+	}
+
 }
