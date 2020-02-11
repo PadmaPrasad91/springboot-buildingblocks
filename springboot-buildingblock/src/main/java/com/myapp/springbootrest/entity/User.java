e4a0629699aeff0c2572b220e8090fedde87e882
PadmaPrasad <padmaprasad91@gmail.com>
2020-02-11 22:20:51 +0530

    JsonView first commit

--- springboot-buildingblock/src/main/java/com/myapp/springbootrest/entity/User.java
+++ springboot-buildingblock/src/main/java/com/myapp/springbootrest/entity/User.java
@@ -12,39 +12,46 @@ import javax.validation.constraints.Size;
 
 import org.springframework.hateoas.RepresentationModel;
 
-import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonView;
 
 @Entity
-@JsonFilter(value = "userFilter")
+//@JsonFilter(value = "userFilter") -- used for jsonMapped filter
 public class User extends RepresentationModel {
 
 	@Id
 	@GeneratedValue
+	@JsonView(Views.External.class)
 	private Long userid;
 
 	@NotEmpty(message = "Username is mandatory field and should not be empty")
 	@Column(name = "USER_NAME", length = 50, nullable = false, unique = true)
+	@JsonView(Views.External.class)
 	private String username;
 
 	@Size(min = 2, message = "Firstname should contain atleast 2 characters")
 	@Column(name = "FIRST_NAME", length = 50, nullable = false)
+	@JsonView(Views.External.class)
 	private String firstname;
 
 	@Column(name = "LAST_NAME", length = 50, nullable = false)
+	@JsonView(Views.External.class)
 	private String lastname;
 
 	@Column(name = "EMAIL_ADDRESS", length = 50, nullable = false)
+	@JsonView(Views.External.class)
 	private String email;
 
 	@Column(name = "ROLE", length = 50, nullable = false)
+	@JsonView(Views.Internal.class)
 	private String role;
 
 //	@JsonIgnore   --------static filtering @JsonIgnor
 	@Column(name = "SSN", length = 50, nullable = false, unique = true)
+	@JsonView(Views.Internal.class)
 	private String ssn;
 
 	@OneToMany(mappedBy = "user")
+	@JsonView(Views.Internal.class)
 	private List<Order> orders;
 
 	public User() {
